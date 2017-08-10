2bda3315f42e3f240cdd10fc228b26a72b4ccb33
lcysgsg <249313573@qq.com>
2017-08-10 21:36:54 +0800

    'https://ts.xcatliu.com/basics/any.html'

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -12,3 +12,65 @@ function greeter(person) {
 }
 var user = new Student('Jasne', 'M.', 'User');
 document.body.innerHTML = greeter(user);
+/**
+ * 使用 number 定义字符串类型
+ */
+var decLiteral = 6;
+var hexLiteral = 0xf00d;
+// ES6 中的二进制表示法, 0b1010
+var binaryLiteral = 10;
+// ES6 中的八进制表示法, 0o744
+var octalLiteral = 484;
+var notANumber = NaN;
+var infinityNumber = Infinity;
+/**
+ * 使用 string 定义字符串类型：
+ */
+var myName = 'Chao Yang';
+var myAge = 20;
+// 模板字符串
+var sentence = "Hello, my name is " + myName + ".\nI'll be " + (myAge + 1) + " years old next month.";
+/**
+ * JavaScript 没有空值（Void）的概念
+ * 在 TypeScirpt 中，可以用 void 表示没有任何返回值的函数：
+ */
+function alertName() {
+    alert('My name is CHAOYANG');
+}
+// 声明一个 void 类型的变量没有什么用
+// 因为你只能将它赋值为 undefined 和 null：
+var unusable = undefined;
+// 在 TypeScript 中，可以使用 null 和 undefined 来定义这两个原始数据类型：
+// undefined 类型的变量只能被赋值为 undefined
+// null 类型的变量只能被赋值为 null
+var u = undefined;
+var n = null;
+// 与 void 的区别是，undefined 和 null 是所有类型的子类型。也就是说 undefined 类型的变量，可以赋值给 number 类型的变量：
+var num = u;
+// 而 void 类型的变量不能赋值给 number 类型的变量
+/**
+ * 任意值类型
+ * 如果是一个普通类型，在赋值过程中改变类型是不被允许的
+ * 但如果是 any 类型，则允许被赋值为任意类型
+ */
+var myFavoriteNumber = 'seven';
+myFavoriteNumber = 7;
+// 任意值的属性和方法
+// 在任意值上访问任何属性都是允许的：
+var anyThing = {
+    myName: 'lcy'
+};
+var firstName = {
+    myFirstName: 'Li',
+    age: 20
+};
+console.log(anyThing.myName);
+console.log(firstName.myFirstName + firstName.age);
+// 变量如果在声明的时候，未指定其类型，那么它会被识别为任意值类型
+var something1;
+something1 = 'seven';
+something1 = 7;
+// 等于
+var something2;
+something2 = 'seven';
+something2 = 7;
--- greeter/greeter.ts
+++ greeter/greeter.ts
@@ -7,8 +7,7 @@ class Student {
 
 interface Person {
   firstName: string,
-  lastName: string,
-  number: number
+  lastName: string
 }
 
 function greeter(person: Person) {
@@ -19,6 +18,72 @@ var user = new Student('Jasne', 'M.', 'User')
 
 document.body.innerHTML = greeter(user)
 
+ 
+/**
+ * 使用 number 定义字符串类型
+ */
+let decLiteral: number = 6
+let hexLiteral: number = 0xf00d
+// ES6 中的二进制表示法, 0b1010
+let binaryLiteral: number = 0b1010
+// ES6 中的八进制表示法, 0o744
+let octalLiteral: number = 0o744
+let notANumber: number = NaN
+let infinityNumber: number = Infinity
 
-let decLiteral: numbner = 6;
+/**
+ * 使用 string 定义字符串类型：
+ */
+let myName: string = 'Chao Yang';
+let myAge: number = 20;
+// 模板字符串
+let sentence: string = `Hello, my name is ${myName}.
+I'll be ${myAge + 1} years old next month.`;
 
+/**
+ * JavaScript 没有空值（Void）的概念
+ * 在 TypeScirpt 中，可以用 void 表示没有任何返回值的函数：
+ */
+function alertName(): void {
+  alert('My name is CHAOYANG');
+}
+// 声明一个 void 类型的变量没有什么用
+// 因为你只能将它赋值为 undefined 和 null：
+let unusable: void = undefined;
+// 在 TypeScript 中，可以使用 null 和 undefined 来定义这两个原始数据类型：
+// undefined 类型的变量只能被赋值为 undefined
+// null 类型的变量只能被赋值为 null
+let u: undefined = undefined;
+let n: null = null;
+// 与 void 的区别是，undefined 和 null 是所有类型的子类型。也就是说 undefined 类型的变量，可以赋值给 number 类型的变量：
+let num: number = u;
+// 而 void 类型的变量不能赋值给 number 类型的变量
+
+/**
+ * 任意值类型
+ * 如果是一个普通类型，在赋值过程中改变类型是不被允许的
+ * 但如果是 any 类型，则允许被赋值为任意类型
+ */
+let myFavoriteNumber: any = 'seven';
+myFavoriteNumber = 7;
+
+// 任意值的属性和方法
+// 在任意值上访问任何属性都是允许的：
+let anyThing: any = {
+  myName: 'lcy'
+}
+let firstName: object = {
+  myFirstName: 'Li',
+  age: 20
+}
+console.log(anyThing.myName);
+console.log(firstName.myFirstName+ firstName.age);
+
+// 变量如果在声明的时候，未指定其类型，那么它会被识别为任意值类型
+let something1;
+something1 = 'seven';
+something1 = 7;
+// 等于
+let something2: any;
+something2 = 'seven';
+something2 = 7;
